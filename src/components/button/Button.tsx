9462a4adbc0cbeb4fa290cd35b3b14ef3b27d51c
BrandonWu500 <brwu17@gmail.com>
2023-09-05 12:10:14 -0400

    feat: add default button styles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,9 @@
-type Props = {};
+import { VariantProps, cva } from 'cva';
+
+const buttonStyles = cva(`uppercase text-neutral-50 font-bold`);
+
+type Props = VariantProps<typeof buttonStyles> & {};
 const Button = (props: Props) => {
-  return <div>Button</div>;
+  return <button className={buttonStyles()}>Button</button>;
 };
 export default Button;
