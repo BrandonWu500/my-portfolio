9e34f20e15fd316410a461008feacb5770e60e17
BrandonWu500 <brwu17@gmail.com>
2023-09-05 12:21:56 -0400

    test: button intent variant

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -5,7 +5,10 @@ import Button from './Button';
 const meta = {
   title: 'Button',
   component: Button,
-
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
 } satisfies Meta<typeof Button>;
@@ -15,5 +18,13 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
-  args: {},
+  args: {
+    intent: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    intent: 'secondary',
+  },
 };
