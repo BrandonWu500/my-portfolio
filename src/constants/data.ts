3ec94767ed643f01c4bf1d0632bc998d49001557
BrandonWu500 <brwu17@gmail.com>
2023-09-06 15:36:37 -0400

    feat: add project data

---
+++ src/constants/data.ts
@@ -0,0 +1,23 @@
+import { ProjectType } from '@/types';
+import carRentalThumbnail from '/public/car-rental-project-thumbnail.png';
+
+export const projects: ProjectType[] = [
+  {
+    imgUrl: carRentalThumbnail,
+    title: 'Car Rental Web App',
+    description:
+      'A personal project I created with a similar design to Airbnb.',
+    codeLink: 'https://github.com/BrandonWu500/car-rental-web-app',
+    deployLink: 'https://brandonwu500-car-rental-web-app-v1.vercel.app',
+  },
+  {
+    imgUrl: carRentalThumbnail,
+    title: 'My Portfolio',
+    description:
+      'My personal portfolio, which is the website you are currently viewing!',
+    codeLink: 'https://github.com/BrandonWu500/my-portfolio',
+    deployLink: '',
+    designLink:
+      'https://www.figma.com/file/WnCAyzR6Wf6o70u9D2JOp9/My-Portfolio-Web-Design?type=design&node-id=109%3A8334&mode=design&t=s4uJvIVlACkYJkgZ-1',
+  },
+];
