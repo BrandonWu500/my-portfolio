237b33bea3a0911b92e7f098335e084df7dc015d
BrandonWu500 <brwu17@gmail.com>
2023-09-06 10:32:25 -0400

    feat: add heading and body fonts

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { Besley, Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -5,10 +6,15 @@ import mobileNavIcon from '/public/bars-3.svg';
 import desktopLogo from '/public/desktop-logo.svg';
 import mobileLogo from '/public/mobile-logo.svg';
 
+const besley = Besley({ subsets: ['latin'], variable: '--heading-font' });
+const inter = Inter({ subsets: ['latin'], variable: '--body-font' });
+
 type Props = {};
 const Header = (props: Props) => {
   return (
-    <div className="sticky top-0 flex w-screen items-center justify-between bg-neutral-900 p-4 text-neutral-50">
+    <div
+      className={`${besley.variable} ${inter.variable} sticky top-0 flex w-screen items-center justify-between bg-neutral-900 p-4 text-neutral-50`}
+    >
       <Link href={'/'}>
         <Image src={mobileLogo} alt="logo" className="lg:hidden" />
         <Image src={desktopLogo} alt="logo" className="hidden lg:block" />
