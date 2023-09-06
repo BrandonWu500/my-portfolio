a446923963f1ce24146484a03add512e00f9077d
BrandonWu500 <brwu17@gmail.com>
2023-09-06 09:16:45 -0400

    feat: add logo to header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
+import desktopLogo from '/public/desktop-logo.svg';
+import mobileLogo from '/public/mobile-logo.svg';
+
 type Props = {};
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className="sticky top-0 w-screen bg-neutral-900 p-4 text-neutral-50">
+      {/* MOBILE LOGO */}
+      <Image src={mobileLogo} alt="logo" className="lg:hidden" />
+      {/* DESKTOP LOGO */}
+      <Image src={desktopLogo} alt="logo" className="hidden lg:block" />
+    </div>
+  );
 };
 export default Header;
