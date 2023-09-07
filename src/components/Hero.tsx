94ec894ef3e0e7b03a676e7a816b8dfcbfc34e22
BrandonWu500 <brwu17@gmail.com>
2023-09-07 15:00:31 -0400

    feat: add hero image and text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,26 @@
+import Image from 'next/image';
+
+import headshot from '/public/my-portrait.jpg';
+
 type Props = {};
 const Hero = (props: Props) => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex flex-col items-center gap-12 bg-neutral-900 px-12 py-7 text-neutral-50">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={headshot}
+          alt="my headshot"
+          width={100}
+          height={100}
+          className="relative h-[100px] w-[100px] rounded-full border-[3px] border-neutral-50 object-cover"
+          priority
+        />
+        <h1 className="font-heading text-[40px] font-bold">Brandon Wu</h1>
+        <h3 className="font-heading font-semibold text-neutral-100">
+          {`Web Designer & Web Developer`}
+        </h3>
+      </div>
+    </div>
+  );
 };
 export default Hero;
