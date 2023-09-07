bd3f12cc6823781a09ef26217cd73c5fbbc885c8
BrandonWu500 <brwu17@gmail.com>
2023-09-07 13:28:34 -0400

    feat: add size variant

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -1,15 +1,17 @@
 import { VariantProps, cva } from 'cva';
 import { Inter } from 'next/font/google';
 import Image, { StaticImageData } from 'next/image';
+import { useMemo } from 'react';
 
 const inter = Inter({ subsets: ['latin'], variable: '--body-font' });
 
 const skillStyles = cva(
-  `${inter.variable} bg-neutral-200 text-neutral-900 font-body flex flex-col items-center gap-2 p-2`,
+  `${inter.variable} font-body flex flex-col items-center gap-2 p-2`,
   {
     variants: {
       size: {
-        small: '',
+        small: 'bg-neutral-200 text-neutral-900 text-xs',
+        large: 'bg-neutral-700 text-neutral-50',
       },
     },
   }
@@ -20,10 +22,25 @@ type Props = VariantProps<typeof skillStyles> & {
   label: string;
 };
 const Skill = ({ size = 'small', svgIcon, label }: Props) => {
+  const svgSize = useMemo(() => {
+    if (!svgIcon) return;
+
+    switch (size) {
+      case 'small':
+        return 24;
+
+      case 'large':
+        return 48;
+
+      default:
+        return 24;
+    }
+  }, [size, svgIcon]);
+
   return (
     <div className={skillStyles({ size })}>
-      <Image src={svgIcon} alt="skill icon" width={32} height={32} />
-      <p className="text-xs font-medium">{label}</p>
+      <Image src={svgIcon} alt="skill icon" width={svgSize} height={svgSize} />
+      <p className="font-medium">{label}</p>
     </div>
   );
 };
