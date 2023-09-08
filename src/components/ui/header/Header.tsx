9397a3ff883544c595c73931b35ceba8353e55cd
BrandonWu500 <brwu17@gmail.com>
2023-09-08 08:15:24 -0400

    feat: add mobile nav links

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -14,7 +14,7 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div
-      className={`${besley.variable} ${inter.variable} sticky top-0 z-50 flex w-screen items-center justify-between bg-neutral-900 p-4 text-neutral-50`}
+      className={`${besley.variable} ${inter.variable} sticky top-0 z-50 flex h-24 w-screen items-center justify-between bg-neutral-900 p-8 text-neutral-50`}
     >
       <Link href={'/'}>
         <Image src={mobileLogo} alt="logo" className="lg:hidden" />
@@ -22,6 +22,27 @@ const Header = (props: Props) => {
       </Link>
 
       <Image src={mobileNavIcon} alt="menu" className="lg:hidden" />
+
+      {/* MOBILE NAV */}
+      <div className="fixed left-0 top-0 z-50 mt-24 h-screen bg-neutral-900">
+        <ul className="flex w-[250px] flex-col gap-12 p-8 pt-6 font-heading text-2xl font-semibold">
+          <li>
+            <Link href={'#hero'}>Home</Link>
+          </li>
+          <li>
+            <Link href={'#about'}>About</Link>
+          </li>
+          <li>
+            <Link href={'#skills'}>Skills</Link>
+          </li>
+          <li>
+            <Link href={'#projects'}>Projects</Link>
+          </li>
+          <li>
+            <Link href={'#contact'}>Contact</Link>
+          </li>
+        </ul>
+      </div>
 
       <div className="hidden items-center gap-[72px] lg:flex">
         {/* DESKTOP NAV */}
