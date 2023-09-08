7a1365fdb5e2890368e293a88592e13589a5c24f
BrandonWu500 <brwu17@gmail.com>
2023-09-08 08:26:38 -0400

    feat: add mobile nav toggle

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,17 +1,24 @@
+'use client';
+
 import { Besley, Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import Button from '../button/Button';
+
 import mobileNavIcon from '/public/bars-3.svg';
 import desktopLogo from '/public/desktop-logo.svg';
 import mobileLogo from '/public/mobile-logo.svg';
+import closeIcon from '/public/x-circle.svg';
 
 const besley = Besley({ subsets: ['latin'], variable: '--heading-font' });
 const inter = Inter({ subsets: ['latin'], variable: '--body-font' });
 
 type Props = {};
 const Header = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div
       className={`${besley.variable} ${inter.variable} sticky top-0 z-50 flex h-24 w-screen items-center justify-between bg-neutral-900 p-8 text-neutral-50`}
@@ -21,28 +28,36 @@ const Header = (props: Props) => {
         <Image src={desktopLogo} alt="logo" className="hidden lg:block" />
       </Link>
 
-      <Image src={mobileNavIcon} alt="menu" className="lg:hidden" />
+      <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          <Image src={closeIcon} alt="close" width={48} height={48} />
+        ) : (
+          <Image src={mobileNavIcon} alt="menu" />
+        )}
+      </button>
 
       {/* MOBILE NAV */}
-      <div className="fixed left-0 top-0 z-50 mt-24 h-screen bg-neutral-900">
-        <ul className="flex w-[250px] flex-col gap-12 p-8 pt-6 font-heading text-2xl font-semibold">
-          <li>
-            <Link href={'#hero'}>Home</Link>
-          </li>
-          <li>
-            <Link href={'#about'}>About</Link>
-          </li>
-          <li>
-            <Link href={'#skills'}>Skills</Link>
-          </li>
-          <li>
-            <Link href={'#projects'}>Projects</Link>
-          </li>
-          <li>
-            <Link href={'#contact'}>Contact</Link>
-          </li>
-        </ul>
-      </div>
+      {isOpen && (
+        <div className="fixed left-0 top-0 z-50 mt-24 h-screen bg-neutral-900">
+          <ul className="flex w-[250px] flex-col gap-12 p-8 pt-6 font-heading text-2xl font-semibold">
+            <li>
+              <Link href={'#hero'}>Home</Link>
+            </li>
+            <li>
+              <Link href={'#about'}>About</Link>
+            </li>
+            <li>
+              <Link href={'#skills'}>Skills</Link>
+            </li>
+            <li>
+              <Link href={'#projects'}>Projects</Link>
+            </li>
+            <li>
+              <Link href={'#contact'}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      )}
 
       <div className="hidden items-center gap-[72px] lg:flex">
         {/* DESKTOP NAV */}
