7ff18c4d4215de73091e32f8af8db716aff1fa84
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-08 09:41:27 -0400

    feat: create mobile nav (#20)

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,47 +1,79 @@
+'use client';
+
 import { Besley, Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import { navLinks } from '../../../constants/data';
 
 import Button from '../button/Button';
+
 import mobileNavIcon from '/public/bars-3.svg';
 import desktopLogo from '/public/desktop-logo.svg';
 import mobileLogo from '/public/mobile-logo.svg';
+import closeIcon from '/public/x-circle.svg';
 
 const besley = Besley({ subsets: ['latin'], variable: '--heading-font' });
 const inter = Inter({ subsets: ['latin'], variable: '--body-font' });
 
 type Props = {};
 const Header = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div
-      className={`${besley.variable} ${inter.variable} sticky top-0 z-50 flex w-screen items-center justify-between bg-neutral-900 p-4 text-neutral-50`}
+      className={`${besley.variable} ${inter.variable} sticky top-0 z-50 flex h-24 w-screen items-center justify-between bg-neutral-900 p-8 text-neutral-50`}
     >
-      <Link href={'/'}>
+      <Link href={'/'} onClick={() => setIsOpen(false)}>
         <Image src={mobileLogo} alt="logo" className="lg:hidden" />
         <Image src={desktopLogo} alt="logo" className="hidden lg:block" />
       </Link>
 
-      <Image src={mobileNavIcon} alt="menu" className="lg:hidden" />
+      <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          <Image src={closeIcon} alt="close" width={48} height={48} />
+        ) : (
+          <Image src={mobileNavIcon} alt="menu" />
+        )}
+      </button>
+
+      {/* MOBILE NAV OVERLAY */}
+      {isOpen && (
+        <div
+          className={
+            'fixed left-0 top-0 z-40 mt-24 h-screen w-full bg-neutral-900/80'
+          }
+          onClick={() => setIsOpen(false)}
+        ></div>
+      )}
+
+      {/* MOBILE NAV */}
+      <div
+        className={twMerge(
+          'fixed -left-[250px] top-0 z-50 mt-24 h-screen w-[250px] bg-neutral-900 transition-transform duration-500 ease-in-out',
+          isOpen && 'translate-x-[250px]'
+        )}
+      >
+        <ul className="flex flex-col gap-12 p-8 pt-6 font-heading text-2xl font-semibold">
+          {navLinks.map((link) => (
+            <li key={link.label} onClick={() => setIsOpen(false)}>
+              <Link href={link.url}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div className="hidden items-center gap-[72px] lg:flex">
         {/* DESKTOP NAV */}
         <nav>
           <ul className="flex items-center gap-[72px] font-heading font-semibold xl:text-2xl">
-            <li>
-              <Link href={'#hero'}>Home</Link>
-            </li>
-            <li>
-              <Link href={'#about'}>About</Link>
-            </li>
-            <li>
-              <Link href={'#skills'}>Skills</Link>
-            </li>
-            <li>
-              <Link href={'#projects'}>Projects</Link>
-            </li>
-            <li>
-              <Link href={'#contact'}>Contact</Link>
-            </li>
+            {navLinks.map((link) => (
+              <li key={link.label}>
+                <Link href={link.url}>{link.label}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
