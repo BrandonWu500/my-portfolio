5590a8a384f5caa26225acf126348d04a00d695c
BrandonWu500 <brwu17@gmail.com>
2023-09-08 11:34:08 -0400

    feat: add about me header content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,20 @@
 type Props = {};
 const About = (props: Props) => {
-  return <div>About</div>;
+  return (
+    <div
+      id="about"
+      className="flex w-screen flex-col gap-8 bg-neutral-900 px-8 py-4 text-neutral-50"
+    >
+      <h2 className="text-center font-heading text-[32px] font-semibold">
+        About Me
+      </h2>
+      <div className="mx-auto flex max-w-[315px] flex-col gap-5">
+        <p className="font-heading text-xl font-medium">
+          Hi, my name is Brandon!
+        </p>
+        <p className="font-body text-lg font-medium">{`I'm a self-taught web designer and web developer.`}</p>
+      </div>
+    </div>
+  );
 };
 export default About;
