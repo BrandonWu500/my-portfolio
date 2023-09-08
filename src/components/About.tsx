457f5e4614caeda1c4786d43d38e7a060c008ad6
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-08 19:00:53 -0400

    feat: add desktop about me (#24)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,19 +10,30 @@ const About = (props: Props) => {
       id="about"
       className="w-screen bg-neutral-900 px-8 py-4 text-neutral-50"
     >
-      <div className="mx-auto flex max-w-[330px] flex-col items-center gap-8">
-        <h2 className="text-center font-heading text-[32px] font-semibold">
-          About Me
-        </h2>
-        <div className="flex flex-col gap-5">
-          <p className="font-heading text-xl font-medium">
+      <h2 className="mb-8 text-center font-heading text-[32px] font-semibold xl:my-16">
+        About Me
+      </h2>
+      <div className="mx-auto flex max-w-[330px] flex-col items-center gap-8 xl:grid xl:max-w-[1000px] xl:grid-cols-2 xl:justify-center">
+        <div className="flex flex-col gap-5 xl:gap-8">
+          <p className="font-heading text-xl font-medium xl:text-4xl">
             Hi, my name is Brandon!
           </p>
-          <p className="font-body text-lg font-medium">
+          <p className="font-body text-lg font-medium xl:mb-5 xl:text-2xl">
             {`I'm a self-taught web designer and web developer.`}
           </p>
+          <a
+            href={'/Resume-Brandon-Wu.pdf'}
+            download
+            className="hidden w-full xl:block"
+          >
+            <Button
+              svgIcon={arrowDownTray}
+              label="Download Resume"
+              className="justify-center"
+            />
+          </a>
         </div>
-        <div className="bg-neutral-700/50 px-8 py-4 font-body">
+        <div className="bg-neutral-700/50 px-8 py-4 font-body xl:text-lg">
           <p className="mb-2">I have:</p>
           <ul className="list-disc space-y-4 pl-4">
             {experience.map((item) => (
@@ -30,7 +41,11 @@ const About = (props: Props) => {
             ))}
           </ul>
         </div>
-        <a href={'/Resume-Brandon-Wu.pdf'} download className="w-full">
+        <a
+          href={'/Resume-Brandon-Wu.pdf'}
+          download
+          className="w-full xl:hidden"
+        >
           <Button
             svgIcon={arrowDownTray}
             label="Download Resume"
