d299209fe430eae9067793f861690b52e82a43a5
BrandonWu500 <brwu17@gmail.com>
2023-09-08 18:58:34 -0400

    feat: increase font for desktop

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,15 +10,15 @@ const About = (props: Props) => {
       id="about"
       className="w-screen bg-neutral-900 px-8 py-4 text-neutral-50"
     >
-      <h2 className="mb-8 text-center font-heading text-[32px] font-semibold xl:mb-16">
+      <h2 className="mb-8 text-center font-heading text-[32px] font-semibold xl:my-16">
         About Me
       </h2>
       <div className="mx-auto flex max-w-[330px] flex-col items-center gap-8 xl:grid xl:max-w-[1000px] xl:grid-cols-2 xl:justify-center">
-        <div className="flex flex-col gap-5">
-          <p className="font-heading text-xl font-medium">
+        <div className="flex flex-col gap-5 xl:gap-8">
+          <p className="font-heading text-xl font-medium xl:text-4xl">
             Hi, my name is Brandon!
           </p>
-          <p className="font-body text-lg font-medium">
+          <p className="font-body text-lg font-medium xl:mb-5 xl:text-2xl">
             {`I'm a self-taught web designer and web developer.`}
           </p>
           <a
@@ -33,7 +33,7 @@ const About = (props: Props) => {
             />
           </a>
         </div>
-        <div className="bg-neutral-700/50 px-8 py-4 font-body">
+        <div className="bg-neutral-700/50 px-8 py-4 font-body xl:text-lg">
           <p className="mb-2">I have:</p>
           <ul className="list-disc space-y-4 pl-4">
             {experience.map((item) => (
