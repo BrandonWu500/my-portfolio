c9241ac925dd5a8843f21f2c84d168b9df0d7dcc
BrandonWu500 <brwu17@gmail.com>
2023-09-09 13:50:00 -0400

    feat: add contact me content

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
+
 import SectionTitle from '@/components/SectionTitle';
+import { mySocialLinks } from '@/constants/data';
 
 type Props = {};
 const Contact = (props: Props) => {
@@ -8,6 +11,15 @@ const Contact = (props: Props) => {
       className="w-screen bg-neutral-900 px-8 py-4 text-neutral-50"
     >
       <SectionTitle title="Contact Me" />
+      <div className="mx-auto flex max-w-[250px] flex-col items-center gap-8 font-body">
+        <p className="font-medium">
+          The best way to reach me is to send me an email.
+        </p>
+        <div className="flex items-center gap-3">
+          <Image src={mySocialLinks.email.icon} alt="mail icon" />
+          <p className="font-semibold">brandonwu500@gmail.com</p>
+        </div>
+      </div>
     </div>
   );
 };
