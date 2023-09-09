37ec870cede0041060acbfb8a8f05371d55b54ef
BrandonWu500 <brwu17@gmail.com>
2023-09-09 11:32:11 -0400

    feat: add title and project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,18 @@
+import SectionTitle from '@/components/SectionTitle';
+import ProjectCard from '@/components/ui/card/ProjectCard';
+import { projects } from '@/constants/data';
+
 type Props = {};
 const Projects = (props: Props) => {
-  return <div>Projects</div>;
+  return (
+    <div id="projects" className="w-screen bg-neutral-900 py-4 text-neutral-50">
+      <SectionTitle title="Projects" />
+      <div className="flex flex-col items-center gap-8">
+        {projects.map((project) => (
+          <ProjectCard key={project.title} {...project} />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Projects;
