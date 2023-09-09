d75fa99032e3c0ce6d23582e899c4cb6ac8e8155
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-09 11:33:16 -0400

    feat: create mobile projects section (#27)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from '@/components/About';
 import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 
 type Props = {};
@@ -9,6 +10,7 @@ const HomePage = (props: Props) => {
       <Hero />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 };
