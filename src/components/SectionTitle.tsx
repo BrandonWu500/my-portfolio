0582f93e65770e922916915a74ac05a22e8877ae
BrandonWu500 <brwu17@gmail.com>
2023-09-09 09:38:55 -0400

    fix: section title desktop font size

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 const SectionTitle = ({ title }: Props) => {
   return (
-    <h2 className="mb-8 text-center font-heading text-[32px] font-semibold xl:my-16">
+    <h2 className="mb-8 text-center font-heading text-[32px] font-semibold xl:my-16 xl:text-5xl">
       {title}
     </h2>
   );
