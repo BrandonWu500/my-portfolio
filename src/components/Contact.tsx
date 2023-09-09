4731ff5326518b21de934b9625945f0b3918eb3e
BrandonWu500 <brwu17@gmail.com>
2023-09-09 13:28:01 -0400

    feat: add title

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,14 @@
+import SectionTitle from '@/components/SectionTitle';
+
 type Props = {};
 const Contact = (props: Props) => {
-  return <div>Contact</div>;
+  return (
+    <div
+      id="contact"
+      className="w-screen bg-neutral-900 px-8 py-4 text-neutral-50"
+    >
+      <SectionTitle title="Contact Me" />
+    </div>
+  );
 };
 export default Contact;
