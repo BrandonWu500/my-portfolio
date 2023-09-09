07d3bd7d81cb9062536ab48a5dbea883ba66ce64
BrandonWu500 <brwu17@gmail.com>
2023-09-09 12:27:55 -0400

    feat: create laptop responsive layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = (props: Props) => {
   return (
     <div id="projects" className="w-screen bg-neutral-900 py-4 text-neutral-50">
       <SectionTitle title="Projects" />
-      <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center gap-8 lg:flex-row lg:justify-center lg:gap-16">
         {projects.map((project) => (
           <ProjectCard key={project.title} {...project} />
         ))}
