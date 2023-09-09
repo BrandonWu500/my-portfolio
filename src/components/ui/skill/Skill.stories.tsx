cd2c351a7a8dcdfb3634cbdd17e898ebfaef53f0
BrandonWu500 <brwu17@gmail.com>
2023-09-09 09:04:20 -0400

    refactor: change import because renamed file

--- src/components/ui/skill/Skill.stories.tsx
+++ src/components/ui/skill/Skill.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Skill from './Skill';
-import storybookIcon from '/public/devicon_storybook.svg';
+import storybookIcon from '/public/storybook.svg';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
