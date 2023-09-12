8bea12752dac4e1ee2e68bfba3652f285891eb83
BrandonWu500 <brwu17@gmail.com>
2023-09-12 18:03:59 -0400

    feat: add animations to contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useMemo } from 'react';
@@ -30,7 +31,19 @@ const Contact = (props: Props) => {
       className="relative w-full snap-center bg-neutral-900 pt-4 text-neutral-50 lg:flex lg:h-screen lg:flex-col lg:justify-center lg:pt-0"
     >
       <SectionTitle title="Contact Me" />
-      <div className="mx-auto flex max-w-[250px] flex-col items-center gap-8 font-body lg:mb-24 lg:max-w-none xl:mb-48 xl:gap-16">
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 2,
+        }}
+        viewport={{ once: true }}
+        className="mx-auto flex max-w-[250px] flex-col items-center gap-8 font-body lg:mb-24 lg:max-w-none xl:mb-48 xl:gap-16"
+      >
         <p className="font-medium lg:text-lg xl:mt-8 xl:text-3xl">
           The best way to reach me is to send me an email.
         </p>
@@ -45,9 +58,21 @@ const Contact = (props: Props) => {
             brandonwu500@gmail.com
           </p>
         </div>
-      </div>
+      </motion.div>
 
-      <footer className="mt-16 w-full bg-neutral-700 p-2 pb-5 lg:absolute lg:bottom-0 lg:pb-2">
+      <motion.footer
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 2,
+        }}
+        viewport={{ once: true }}
+        className="mt-16 w-full bg-neutral-700 p-2 pb-5 lg:absolute lg:bottom-0 lg:pb-2"
+      >
         <div className="flex w-full flex-col items-center gap-8 lg:container lg:mx-auto lg:grid lg:h-20 lg:grid-cols-3 lg:place-items-center xl:h-[100px] xl:px-16">
           <Link href={'#hero'} className="lg:order-3">
             <Button
@@ -75,7 +100,7 @@ const Contact = (props: Props) => {
             © 2023 Brandon Wu. All rights reserved.
           </p>
         </div>
-      </footer>
+      </motion.footer>
     </div>
   );
 };
