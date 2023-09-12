aae8f42c16500c884f03317c0ae964447993beff
BrandonWu500 <brwu17@gmail.com>
2023-09-12 10:03:25 -0400

    feat: increase laptop about me max width

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ const About = (props: Props) => {
   return (
     <div id="about" className="w-full bg-neutral-900 px-8 py-4 text-neutral-50">
       <SectionTitle title="About Me" />
-      <div className="mx-auto flex max-w-[330px] flex-col items-center gap-8 xl:grid xl:max-w-[1000px] xl:grid-cols-2 xl:justify-center">
+      <div className="mx-auto flex max-w-[330px] flex-col items-center gap-8 lg:max-w-md xl:grid xl:max-w-[1000px] xl:grid-cols-2 xl:justify-center">
         <div className="flex flex-col gap-5 xl:gap-8">
           <p className="font-heading text-xl font-medium xl:text-4xl">
             Hi, my name is Brandon!
