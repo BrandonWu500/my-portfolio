19ff253ddf9816cbf23da7c62029519d5a7c1563
BrandonWu500 <brwu17@gmail.com>
2023-09-12 12:08:34 -0400

    feat: add scroll snap to desktop projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,10 +13,10 @@ const Projects = (props: Props) => {
   return (
     <div
       id="projects"
-      className="relative z-10 w-full bg-neutral-900 py-4 text-neutral-50 lg:h-[500px] xl:h-[1024px]"
+      className="relative z-10 w-full snap-center bg-neutral-900 py-4 text-neutral-50 lg:flex lg:h-screen lg:flex-col lg:pt-24"
     >
       <SectionTitle title="Projects" />
-      <div className="flex flex-col items-center gap-8 lg:flex-row lg:justify-center lg:gap-[56px] xl:mt-[230px]">
+      <div className="flex flex-col items-center gap-8 lg:mt-16 lg:flex-row lg:justify-center lg:gap-[56px] xl:mt-24">
         {projects.map((project) => (
           <ProjectCard
             key={project.title}
@@ -27,7 +27,7 @@ const Projects = (props: Props) => {
       </div>
 
       {/* BACKGROUND DECORATION */}
-      <div className="absolute left-0 right-0 top-[35%] -z-10 mx-auto hidden h-[150px] w-full -skew-y-[8deg] bg-neutral-800 lg:block xl:top-[25%] xl:h-[550px]" />
+      <div className="absolute left-0 right-0 top-[35%] -z-10 mx-auto hidden h-[150px] w-full -skew-y-[8deg] bg-neutral-800 lg:top-[45%] lg:block xl:top-[25%] xl:h-[540px]" />
     </div>
   );
 };
