5eab98ecb1fd4ddd797e85ff108728f8bf9c953a
BrandonWu500 <brwu17@gmail.com>
2023-09-13 10:00:04 -0400

    feat: create ScrollDownIndicator.tsx

---
+++ src/components/ScrollDownIndicator.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { BREAKPOINTS } from '@/constants/breakpoints';
+import useWindowDimensions from '@/hooks/useWindowDimensions';
+
+type Props = {};
+const ScrollDownIndicator = (props: Props) => {
+  const { width } = useWindowDimensions();
+
+  if (width && width < BREAKPOINTS.LG) return;
+
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 2,
+      }}
+      viewport={{ once: true }}
+      className="absolute bottom-10 right-12 z-50 flex items-center gap-4 text-neutral-50 drop-shadow"
+    >
+      <span className="h-[1px] w-8 bg-neutral-50 2xl:w-12"></span>
+      <p className="font-body text-xs font-medium uppercase tracking-[3.5px] 2xl:text-sm">
+        Scroll Down
+      </p>
+    </motion.div>
+  );
+};
+export default ScrollDownIndicator;
