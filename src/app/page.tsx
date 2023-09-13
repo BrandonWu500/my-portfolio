dd300905931e0f6cc30263215c36fbf3a408b764
BrandonWu500 <brwu17@gmail.com>
2023-09-13 10:50:35 -0400

    fix: scroll padding on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const HomePage = (props: Props) => {
   const aboutScrollToRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className="h-screen overflow-y-auto overflow-x-hidden pt-24 lg:snap-y lg:snap-mandatory xl:scrollbar xl:scrollbar-track-neutral-900/80 xl:scrollbar-thumb-neutral-50/30">
+    <div className="h-screen scroll-pt-24 overflow-y-auto overflow-x-hidden pt-24 lg:snap-y lg:snap-mandatory lg:scroll-pt-0 xl:scrollbar xl:scrollbar-track-neutral-900/80 xl:scrollbar-thumb-neutral-50/30">
       <Hero scrollToRef={aboutScrollToRef} />
       <div ref={aboutScrollToRef}>
         <About />
