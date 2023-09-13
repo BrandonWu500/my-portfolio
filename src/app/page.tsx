d14b272d3dcfa8fcbab1742384a3e364d70e7b55
BrandonWu500 <brwu17@gmail.com>
2023-09-13 07:18:33 -0400

    feat: add scroll to ref to about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import About from '@/components/About';
 import Contact from '@/components/Contact';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
+import { useRef } from 'react';
 
 type Props = {};
 const HomePage = (props: Props) => {
+  const scrollToRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="h-screen overflow-y-auto overflow-x-hidden pt-24 lg:snap-y lg:snap-mandatory xl:scrollbar xl:scrollbar-track-neutral-900/80 xl:scrollbar-thumb-neutral-50/30">
-      <Hero />
-      <About />
+      <Hero scrollToRef={scrollToRef} />
+      <div ref={scrollToRef}>
+        <About />
+      </div>
       <Skills />
       <Projects />
       <Contact />
