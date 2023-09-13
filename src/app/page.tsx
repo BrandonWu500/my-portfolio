fd75553e01feb422568c237ac0b394c60696e570
BrandonWu500 <brwu17@gmail.com>
2023-09-13 07:22:39 -0400

    refactor: rename scrollToRef to aboutScrollToRef

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,12 @@ import { useRef } from 'react';
 
 type Props = {};
 const HomePage = (props: Props) => {
-  const scrollToRef = useRef<HTMLDivElement>(null);
+  const aboutScrollToRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className="h-screen overflow-y-auto overflow-x-hidden pt-24 lg:snap-y lg:snap-mandatory xl:scrollbar xl:scrollbar-track-neutral-900/80 xl:scrollbar-thumb-neutral-50/30">
-      <Hero scrollToRef={scrollToRef} />
-      <div ref={scrollToRef}>
+      <Hero scrollToRef={aboutScrollToRef} />
+      <div ref={aboutScrollToRef}>
         <About />
       </div>
       <Skills />
