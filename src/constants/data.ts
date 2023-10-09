bdbf7ed5289f10c35274555b6bf24b7b333cb86a
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-10-08 20:35:02 -0400

    chore: clean up unused files (#52)

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,11 +1,7 @@
-import { ProjectType } from '@/src/types';
-
-import carRentalThumbnail from '/public/car-rental-project-thumbnail.png';
 import figmaIcon from '/public/figma-large.svg';
 import githubIcon from '/public/github.svg';
 import linkedinIcon from '/public/linkedin.svg';
 import mailIcon from '/public/mail.svg';
-import myPortfolioThumbnail from '/public/my-portfolio-thumbnail.png';
 import nextjsIcon from '/public/nextjs.svg';
 import githubDarkIcon from '/public/other-tech/github.svg';
 import jiraIcon from '/public/other-tech/jira.svg';
@@ -15,27 +11,6 @@ import shopifyIcon from '/public/other-tech/shopify.svg';
 import vercelIcon from '/public/other-tech/vercel.svg';
 import tailwindIcon from '/public/tailwind.svg';
 import typescriptIcon from '/public/typescript.svg';
-
-export const projects: ProjectType[] = [
-  {
-    imgUrl: carRentalThumbnail,
-    title: 'Car Rental Web App',
-    description:
-      'A personal project I created with a similar design to Airbnb.',
-    codeLink: 'https://github.com/BrandonWu500/car-rental-web-app',
-    deployLink: 'https://brandonwu500-car-rental-web-app-v1.vercel.app',
-  },
-  {
-    imgUrl: myPortfolioThumbnail,
-    title: 'My Portfolio',
-    description:
-      'My personal portfolio, which is the website you are currently viewing!',
-    codeLink: 'https://github.com/BrandonWu500/my-portfolio',
-    deployLink: 'https://brandonwu500-my-portfolio.vercel.app',
-    designLink:
-      'https://www.figma.com/file/WnCAyzR6Wf6o70u9D2JOp9/My-Portfolio-Web-Design?type=design&node-id=109%3A8334&mode=design&t=s4uJvIVlACkYJkgZ-1',
-  },
-];
 
 export const mySocialLinks = {
   github: {
@@ -76,13 +51,6 @@ export const navLinks = [
     url: '#contact',
     label: 'Contact',
   },
-];
-
-export const experience = [
-  '1 year of experience with web development (using Next.js)',
-  '1 year of experience with web design (using Figma)',
-  '1 year of experience with Git and GitHub',
-  '1 year of experience with CI/CD (using GitHub Actions and Vercel)',
 ];
 
 export const favoriteTech = [
