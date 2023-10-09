fd5194867fa8be80eb77839d61f1f9a1579efc45
BrandonWu500 <brwu17@gmail.com>
2023-10-08 21:11:12 -0400

    feat: adjust projects background decoration position

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -34,14 +34,14 @@ const Projects = async (props: Props) => {
       className="relative z-10 w-full snap-center bg-neutral-900 py-4 text-neutral-50 lg:flex lg:h-screen lg:flex-col lg:pt-24"
     >
       <SectionTitle title="Projects" />
-      <div className="flex flex-col items-center gap-8 lg:mt-16 lg:flex-row lg:justify-center lg:gap-[56px] xl:mt-24">
+      <div className="flex flex-col items-center gap-8 lg:mt-12 lg:flex-row lg:justify-center lg:gap-[56px] xl:mt-24">
         {projects.map((project) => (
           <ProjectCard key={project._id} {...project} />
         ))}
       </div>
 
       {/* BACKGROUND DECORATION */}
-      <div className="absolute left-0 right-0 top-[35%] -z-10 mx-auto hidden h-[150px] w-full -skew-y-[8deg] bg-neutral-800 lg:top-[45%] lg:block xl:top-[25%] xl:h-[540px]" />
+      <div className="absolute left-0 right-0 -z-10 mx-auto hidden h-[150px] w-full -skew-y-[8deg] bg-neutral-800 lg:top-[50%] lg:block xl:top-[27.5%] xl:h-[540px]" />
 
       <ScrollDownIndicator />
     </div>
